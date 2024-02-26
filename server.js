f8f875468886fa3e1fe21125f7083a2b2bcfc4b6
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2024-02-27 05:51:44 +0600

    final

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 /** @format */
 
 const http = require('node:http')
-const hostname = ['192.168.209.69', '127.0.0.1']
+const hostname = [
+  '192.168.0.103',
+  '127.0.0.1',
+  '[::1]',
+  '[fe80::e0ed:619a:2d7a:c145%3]'
+]
 const port = 80
 const server = http.createServer((req, res) => {
   res.statusCode = 200
@@ -9,5 +14,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n')
 })
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+  hostname.forEach(element => console.log(`Server running at http://${element}:${port}/`));
+  // console.log(`Server running at http://${hostname}:${port}/`)
 })
